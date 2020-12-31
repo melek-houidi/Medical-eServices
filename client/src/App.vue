4cde00384d70093bcf953c1694472bdcb050ac3c
melek <melekhouidi001@gmail.com>
2020-12-31 13:09:22 +0100

    mail page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,83 @@
 <template>
-  <div id="app">
-    <Main msg="Welcome to Spicy Hackers Vue.js App"/>
+  <div class="slide-show">
+    <Main
+      v-for="(data, index) in slideData"
+      :key="index"
+      v-bind:img="slideData[index].img"
+      v-bind:myId="index + 1"
+      v-bind:currentId="currentId"
+      v-bind:count="slideCounts"
+      v-on:direction="dir"
+      v-bind:startAnime="startAnime"
+    />
   </div>
 </template>
 
 <script>
-import Main from './components/Main.vue'
-
+import Main from "./components/Main";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Main
-  }
-}
+    Main,
+  },
+
+  methods: {
+    dir: function(myDir) {
+      if (myDir === "right") {
+        if (this.currentId < this.slideCounts) {
+          this.currentId += 1;
+        } else {
+          this.currentId = 1;
+        }
+      } else {
+        if (this.currentId > 1) {
+          this.currentId -= 1;
+        } else {
+          this.currentId = this.slideCounts;
+        }
+      }
+    },
+  },
+  created: function() {
+    setInterval(() => {
+      this.startAnime = true;
+      this.dir("right");
+    }, 3000);
+  },
+  data: function() {
+    return {
+      slideCounts: 3,
+      currentId: 1,
+      startAnime: false,
+
+      slideData: [
+        {
+          img:
+            "https://www.expatica.com/app/uploads/sites/6/2017/05/doctors-in-germany-1920x1080.jpg",
+        },
+        {
+          img:
+            "https://www.expatica.com/app/uploads/sites/8/2014/05/hospitals-in-portugal-1920x1080.jpg",
+        },
+        {
+          img:
+            "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/blog_posts/public-health/1800x1200_pharmacist.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.slide-show {
+  width: 100%;
+  height: 100%;
+  background-color: aquamarine;
 }
 </style>
--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -1,40 +1,191 @@
 <template>
-  <div class="main">
-    <h1>{{ msg }}</h1>
-    <p>
-      Here we are in our App Component.
-    </p>
-    <h3>Ahmed</h3>
-    <h3>Zied</h3>
-    <h3>Melek</h3>
-    <h3>Khaled</h3>
-    
+  <div class="slide" :class="[whereAmI]">
+    <img :src="img" alt="img" class="slide-img" />
+    <div class="slide-bullets">
+      <div
+        v-for="n in count"
+        :key="n"
+        class="slide-bullet"
+        :class="currentId == n ? 'bullet-on' : 'n'"
+      />
+    </div>
+    <div class="bar" :class="{ 'bar-animation': startAnime }"></div>
+    <div class="right-arrow" @click="$emit('direction', 'right')">
+      <div class="arrow-pos-div">
+        <img
+          src="https://cdn1.iconfinder.com/data/icons/arrows-directions-crafticons/48/Arrow_left_arrow_2-512.png"
+          alt="arrow"
+          class="right-arrow-pic"
+        />
+      </div>
+    </div>
+    <div class="left-arrow" @click="$emit('direction', 'left')">
+      <div class="arrow-pos-div">
+        <img
+          src="https://cdn1.iconfinder.com/data/icons/arrows-directions-crafticons/48/Arrow_left_arrow_2-512.png"
+          alt="arrow"
+          class="left-arrow-pic"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'main',
-  props: {
-    msg: String
-  }
-}
+  name: "Main",
+  props: ["data", "img", "myId", "currentId", "count", "startAnime"],
+  computed: {
+    whereAmI: function() {
+      if (this.myId > this.currentId) {
+        return {
+          "slide-right": true,
+          "slide-left": false,
+        };
+      } else if (this.myId < this.currentId) {
+        return {
+          "slide-right": false,
+          "slide-left": true,
+        };
+      } else {
+        return {
+          "slide-right": false,
+          "slide-left": false,
+        };
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.slide {
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: blue;
+  height: 100vh;
+  opacity: 1;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.slide-right {
+  opacity: 0;
+  visibility: hidden;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.slide-left {
+  opacity: 0;
+  visibility: hidden;
 }
-a {
-  color: #42b983;
+.slide-img {
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+}
+.right-arrow {
+  position: absolute;
+  right: -50px;
+  top: 50%;
+  height: 600px;
+  width: 150px;
+  transform: translate(-50%, -50%);
+}
+.left-arrow {
+  position: absolute;
+  left: 100px;
+  top: 50%;
+  height: 600px;
+  width: 150px;
+  transform: translate(-50%, -50%);
+}
+.arrow-pos-div {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.right-arrow-pic {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(180deg);
+  width: 100%;
+  height: 300px;
+  opacity: 0.3;
+  filter: invert(90%);
+  transition: 0.5s;
+}
+.right-arrow-pic:hover {
+  opacity: 1;
+}
+.left-arrow-pic {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 300px;
+  opacity: 0.3;
+  filter: invert(90%);
+  transition: 0.5s;
+}
+.left-arrow-pic:hover {
+  opacity: 1;
+}
+.slide-bullets {
+  width: 100px;
+  height: 30px;
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  transform: translate(-50%, -80px);
+  display: flex;
+  justify-content: space-around;
+  align-self: center;
+}
+.slide-bullet {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: transparent;
+  border: 3px solid #fff;
+  position: relative;
+}
+.slide-bullet::after {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  content: "";
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #fff;
+  opacity: 0;
+  transition: 1s;
+}
+.bullet-on {
+  transform: translate(-50%, -50%) scale=1;
+  opacity: 1;
+  background-color: whitesmoke;
+}
+@keyframes myBar {
+  from {
+    transform: translate(-50%, -30px) scaleX(0) scaleY(0.5);
+  }
+  to {
+    transform: translate(-50%, -30px) scaleX(1) scaleY(1);
+  }
+}
+.bar {
+  background-color: rgba(255, 255, 255, 0.7);
+  width: 90%;
+  height: 2px;
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  animation: myBar 5s infinite;
+}
+.bar-animation {
+  transform-origin: 0 0;
 }
 </style>
